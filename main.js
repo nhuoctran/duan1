abbb219146ffac5dd0a11cc348b7b427645dff11
nhuoctran <nguyen.xfiles@gmail.com>
2025-12-03 11:18:27 +0700

    sửa trước khi đổi

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-alert("co len ban oi")
+let userName ;
+userName = prompt("Mời nhập tên của bạn:");
+console.log("Xin chào " + userName + "! Chào mừng bạn đến với trang web của chúng tôi.");
+
+
+
+
