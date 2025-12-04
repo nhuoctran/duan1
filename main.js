a540b6fd7b6447667215615f34cad2979b567c75
nhuoctran <nguyen.xfiles@gmail.com>
2025-12-04 09:41:18 +0700

    bai1

--- main.js
+++ main.js
@@ -1,6 +1,47 @@
-let userName ;
-userName = prompt("Mời nhập tên của bạn:");
-console.log("Xin chào " + userName + "! Chào mừng bạn đến với trang web của chúng tôi.");
+let x = 10;
+let y = 5;
+let z = x + y;
+console.log(x);
+console.log(y);
+console.log(z);
+// gán +=, -=, *=, /=, %=
+console.log("Kết quả phép gán:+=, -=, *=, /=, %=");
+let m = 10;
+m += 5; // m = m + 5
+console.log("Giá trị của m sau phép gán += 5 là: " + m);
+let n = 8;
+n -= 3; // n = n - 3
+console.log("Giá trị của n sau phép gán -= 3 là: " + n);
+let p = 6;
+p *= 4; // p = p * 4
+console.log("Giá trị của p sau phép gán *= 4 là: " + p);
+let q = 20;
+q /= 5; // q = q / 5
+console.log("Giá trị của q sau phép gán /= 5 là: " + q);
+let r = 15;
+r %= 4; // r = r % 4
+console.log("Giá trị của r sau phép gán %= 4 là: " + r);
+let a = 5;
+let f = 2;
+a-=f+=1;
+console.log("Giá trị của e sau phép gán e-=f+=1 là: " + a);
+// toán tử ++, --
+console.log("Kết quả phép toán ++, --");
+let b = 10;
+b++; // b = b + 1
+console.log("Giá trị của b sau phép toán ++ là: " + b);
+let c = 7;
+c--; // c = c - 1
+console.log("Giá trị của c sau phép toán -- là: " + c); 
+let d = 5;
+console.log("Giá trị của d trước phép toán ++ là: " + d);
+console.log("Giá trị của d sau phép toán ++ là: " + ++d); 
+let e = 3;
+console.log("Giá trị của e trước phép toán -- là: " + e);
+console.log("Giá trị của e sau phép toán -- là: " + --e);
+console.log("Giá trị của e sau phép toán -- là: " + --e);
+
+
 
 
 
